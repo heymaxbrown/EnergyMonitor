488ea94150fc1edd9a1064044385d7c2879a11d1
Max Brown <wheeling51@yahoo.com>
2025-09-24 15:21:46 -0700

    Initial Commit

---
+++ EnergyMonitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnergyMonitor
+//
+//  Created by Max Brown on 9/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EnergyMonitor/EnergyMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnergyMonitorApp.swift
+//  EnergyMonitor
+//
+//  Created by Max Brown on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct EnergyMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
